a05567a0ea2f3e8a66cebccf484b75d27110fa49
Hasan <Hasanaldhahi3@gmail.com>
2022-04-07 19:26:27 +0300

    not finished

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 
 // Write your code here
+
+
+const num1 = 0;
+const num2 = 62;
+let multiply = num1 + num2;
+
+
+// function random() {
+
+//     console.log(Number.isInteger(Math.trunc(Math.random() * 10 + 1)));
+//     return Math.trunc(Math.random() * 10 + 1);
+// }
+
+random = Math.trunc(Math.random() + 1);
+
+// console.log(random());
+
+let num3 = 11;
+let num4 = 7;
+
+let mod = num3 % num4;
+
+console.log(mod)
+
+
+let max = Math.max(1, 2, 3, 4, 20);
